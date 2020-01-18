0c7abd7f4ed06b993364852af13faa9d23aea463
Mohammad Ranjbar <mj.ranjbar.94@gmail.com>
2020-01-18 19:37:55 +0330

    add Cards

--- app/Board.php
+++ app/Board.php
@@ -4,12 +4,20 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Board extends Model
-{
-    protected $fillable = ['name', 'user_id'];
+class Board extends Model {
+
+
+    protected $fillable = ['name','user_id'];
+    
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+    	return $this->belongsTo(User::class);
     }
+
+    public function lists()
+    {
+    	return $this->hasMany(Lists::class);
+    }
+
 }
--- app/User.php
+++ app/User.php
@@ -3,25 +3,24 @@
 namespace App;
 
 use Illuminate\Auth\Authenticatable;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable, Authorizable;
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'username',
-        'email',
-        'api_token',
-        'password',
+        'name', 'email','username','api_token','password'
     ];
+
     /**
      * The attributes excluded from the model's JSON form.
      *
